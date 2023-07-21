19da11b3aee66bd4dc46e51e1c45a61d182631da
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-07-21 23:29:39 +0530

    create workExperience dto

--- src/work-experience/dto/create-work-experience.input.ts
+++ src/work-experience/dto/create-work-experience.input.ts
@@ -1,7 +1,29 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsDate, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 @InputType()
 export class CreateWorkExperienceInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field()
+  @IsNotEmpty()
+  @IsString()
+  company: string;
+
+  @Field()
+  @IsNotEmpty()
+  @IsString()
+  position: string;
+
+  @Field()
+  @IsNotEmpty()
+  @IsDate()
+  startDate: Date;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  @IsDate()
+  endDate?: Date;
+
+  @Field()
+  @IsNotEmpty()
+  userID: string;
 }
