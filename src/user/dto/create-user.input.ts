3e3f76f26e17a401cf5f0b8fa6cafb52d4273093
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-07-21 22:54:06 +0530

    create user dto

--- src/user/dto/create-user.input.ts
+++ src/user/dto/create-user.input.ts
@@ -1,7 +1,41 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field()
+  @IsNotEmpty()
+  username: string;
+
+  @Field()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @Field()
+  @IsNotEmpty()
+  uniEmail: string;
+
+  @Field()
+  @IsNotEmpty()
+  firstName: string;
+
+  @Field()
+  @IsNotEmpty()
+  lastName: string;
+
+  @Field({ nullable: true })
+  address?: string;
+
+  @Field({ nullable: true })
+  contactNumber?: string;
+
+  @Field({ nullable: true })
+  degreeProgram?: string;
+
+  @Field({ nullable: true })
+  schoolLastAttended?: string;
+
+  @Field()
+  onine: boolean;
 }
