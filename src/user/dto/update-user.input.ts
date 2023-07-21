20c17520abee186196626d2e74e6524791029e8d
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-07-21 23:06:12 +0530

    update user dto
    repeated create user dto code

--- src/user/dto/update-user.input.ts
+++ src/user/dto/update-user.input.ts
@@ -1,8 +1,51 @@
+import { IsEmail, IsNotEmpty, IsOptional } from 'class-validator';
 import { CreateUserInput } from './create-user.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { InputType, Field, Int, PartialType, ID } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateUserInput extends PartialType(CreateUserInput) {
-  @Field(() => Int)
-  id: number;
+  @Field(() => ID)
+  @IsNotEmpty()
+  id: string;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  username?: string;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @Field({ nullable: true })
+  @IsEmail()
+  uniEmail?: string;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  firstName?: string;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  lastName?: string;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  address?: string;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  contactNumber?: string;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  degreeProgram?: string;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  schoolLastAttended?: string;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  onine?: boolean;
 }
