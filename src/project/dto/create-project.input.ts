a4713317d0c1dceb167411507fb41989f34acce4
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-07-21 23:54:40 +0530

    create project dto

--- src/project/dto/create-project.input.ts
+++ src/project/dto/create-project.input.ts
@@ -1,7 +1,19 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 @InputType()
 export class CreateProjectInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @Field()
+  @IsNotEmpty()
+  userId: string;
 }
