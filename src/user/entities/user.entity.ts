396a0e11e58112a41f2f4e825fae6236b3d7ec2b
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-07-21 22:28:19 +0530

    user entity gql schema

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,4 +1,15 @@
 import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
+import { Achievement } from 'src/achievement/entities/achievement.entity';
+import { Comment } from 'src/comment/entities/comment.entity';
+import { Interest } from 'src/interest/entities/interest.entity';
+import { Post } from 'src/post/entities/post.entity';
+import { Project } from 'src/project/entities/project.entity';
+import { Reply } from 'src/reply/entities/reply.entity';
+import { Skill } from 'src/skill/entities/skill.entity';
+import { SocialLink } from 'src/social-link/entities/social-link.entity';
+import { Sport } from 'src/sport/entities/sport.entity';
+import { UserGroup } from 'src/user-group/entities/user-group.entity';
+import { WorkExperience } from 'src/work-experience/entities/work-experience.entity';
 
 @ObjectType()
 export class User {
@@ -42,4 +53,37 @@ export class User {
   online: boolean;
 
   //Relationships
+
+  @Field(() => [SocialLink], { nullable: true })
+  socialLinks?: SocialLink[];
+
+  @Field(() => [WorkExperience], { nullable: true })
+  workExperience?: WorkExperience[];
+
+  @Field(() => [Skill], { nullable: true })
+  skills?: Skill[];
+
+  @Field(() => [Project], { nullable: true })
+  projects?: Project[];
+
+  @Field(() => [Interest], { nullable: true })
+  interests?: Interest[];
+
+  @Field(() => [Achievement], { nullable: true })
+  achievements?: Achievement[];
+
+  @Field(() => [Sport], { nullable: true })
+  sports?: Sport[];
+
+  @Field(() => [Post], { nullable: true })
+  posts?: Post[];
+
+  @Field(() => [Comment], { nullable: true })
+  comments?: Comment[];
+
+  @Field(() => [UserGroup], { nullable: true })
+  groups?: UserGroup[];
+
+  @Field(() => [Reply], { nullable: true })
+  replies?: Reply[];
 }
