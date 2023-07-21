690aa9edbce6c33d1c738a23606a170cc45798fe
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-07-21 23:39:37 +0530

    create skill dto

--- src/skill/dto/create-skill.input.ts
+++ src/skill/dto/create-skill.input.ts
@@ -1,7 +1,14 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 @InputType()
 export class CreateSkillInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @Field()
+  @IsNotEmpty()
+  userId: string;
 }
