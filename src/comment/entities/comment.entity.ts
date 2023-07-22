d6d4f5efea758333fdef5ac399a7537d9236904b
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-07-22 21:48:16 +0530

    comment entity

--- src/comment/entities/comment.entity.ts
+++ src/comment/entities/comment.entity.ts
@@ -1,7 +1,36 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
+import { Post } from 'src/post/entities/post.entity';
+import { Reply } from 'src/reply/entities/reply.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @ObjectType()
 export class Comment {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => ID)
+  id: string;
+
+  @Field(() => Date)
+  createdAt: Date;
+
+  @Field()
+  text: string;
+
+  //Relationships
+
+  //Author Relatiosnship
+  @Field(() => User)
+  author: User;
+
+  //AuthorId field (foriegn key)
+  authorId: string;
+
+  //Post Relationship
+  @Field(() => Post)
+  post: Post;
+
+  //postId field (foriegn Key)
+  postId: string;
+
+  //Relationship Reply
+  @Field(() => [Reply], { defaultValue: [] })
+  replies: Reply[];
 }
