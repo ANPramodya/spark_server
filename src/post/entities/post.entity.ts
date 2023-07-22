0c09f9177012424bf0bc2efc0b2df356582d9717
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-07-22 21:18:25 +0530

    Post Entity
    comments have empty array, not null

--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -1,7 +1,42 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
+import { Comment } from 'src/comment/entities/comment.entity';
+import { UserGroup } from 'src/user-group/entities/user-group.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @ObjectType()
 export class Post {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => ID)
+  id: string;
+
+  @Field(() => Date)
+  createdAt: Date;
+
+  @Field()
+  title: string;
+
+  @Field({ nullable: true })
+  content?: string;
+
+  @Field({ nullable: true })
+  imageUrl?: string;
+
+  //Relationships
+
+  //Author Relationship
+  @Field(() => User, { nullable: true })
+  author?: User;
+
+  //authorId field (foriegn key)
+  authorId?: string;
+
+  //userGroup Relatioship
+  @Field(() => UserGroup, { nullable: true })
+  group?: UserGroup;
+
+  //groupID field (foriegn key)
+  groupId?: string;
+
+  //Comments Relationship
+  @Field(() => [Comment], { defaultValue: [] })
+  comments: Comment[];
 }
