5479b5091a118ef245c6a39752fce9ddf4e30e44
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-07-22 20:45:24 +0530

    achievement entity

--- src/achievement/entities/achievement.entity.ts
+++ src/achievement/entities/achievement.entity.ts
@@ -1,7 +1,22 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
+import { User } from 'src/user/entities/user.entity';
 
 @ObjectType()
 export class Achievement {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => ID)
+  id: string;
+
+  @Field()
+  title: string;
+
+  @Field({ nullable: true })
+  descrription?: string;
+
+  //Relationships
+
+  @Field(() => User)
+  user: User;
+
+  //UserId field (foreign key)
+  userId: string;
 }
