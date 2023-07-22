7a8c105bdcea83f66fa298be655f2fa2b99fbf82
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-07-22 21:40:39 +0530

    create post dto

--- src/post/dto/create-post.input.ts
+++ src/post/dto/create-post.input.ts
@@ -1,7 +1,32 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { UserGroup } from 'src/user-group/entities/user-group.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @InputType()
 export class CreatePostInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field()
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
+  content?: string;
+
+  @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
+  imageUrl?: string;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
+  authorId?: string;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
+  groupId: string;
 }
