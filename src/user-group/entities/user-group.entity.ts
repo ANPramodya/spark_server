30a5bb4484def67f49aefca635f5c81fb647a27e
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-07-22 22:01:23 +0530

    userGroup Entity

--- src/user-group/entities/user-group.entity.ts
+++ src/user-group/entities/user-group.entity.ts
@@ -1,7 +1,34 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
+import { Post } from 'src/post/entities/post.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @ObjectType()
 export class UserGroup {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => ID)
+  id: string;
+
+  @Field(() => Date)
+  createdAt: Date;
+
+  @Field(() => Date)
+  updatedAt: Date;
+
+  @Field()
+  name: string;
+
+  @Field({ nullable: true })
+  description?: string;
+
+  @Field({ nullable: true })
+  iconUrl?: string;
+
+  //Relationships
+
+  //User Relationship
+  @Field(() => [User])
+  users: User[]; //the creator will be added in the creation process
+
+  //Post Relationship
+  @Field(() => [Post], { defaultValue: [] })
+  posts: Post[];
 }
