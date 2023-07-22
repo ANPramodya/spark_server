59df5e143cb745480e672d0304c357d635c60f00
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-07-22 20:55:59 +0530

    create sport dto

--- src/sport/dto/create-sport.input.ts
+++ src/sport/dto/create-sport.input.ts
@@ -1,7 +1,15 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 @InputType()
 export class CreateSportInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @Field()
+  @IsNotEmpty()
+  @IsString()
+  userId: string;
 }
