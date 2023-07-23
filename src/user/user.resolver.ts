1cf07541bcb638e8d0c80028ed5837f2d66cda88
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-07-23 22:19:19 +0530

    user service dummy

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -29,7 +29,7 @@ export class UserResolver {
   }
 
   @Mutation(() => User)
-  removeUser(@Args('id', { type: () => Int }) id: string) {
+  removeUser(@Args('id', { type: () => ID }) id: string) {
     return this.userService.remove(id);
   }
 }
